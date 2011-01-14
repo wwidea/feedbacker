3dfc0921e75d167a3cd2ef0c947120aa0be28226
Jonathan Garvin <jgarvin@wwidea.org>
2011-01-14 13:47:58 -0700

    remove deprecated file

--- lib/feedbacker/remark_observer_methods.rb
+++
@@ -1,8 +0,0 @@
-module Feedbacker #:nodoc:
-  module RemarkObserverMethods
-    def after_create(remark)
-      puts '################################################ IN OBSERVER'
-      FeedbackMailer.feedback(remark).deliver
-    end
-  end
-end
