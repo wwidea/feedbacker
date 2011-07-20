52296a285d7b04a8e5c3460c34c67ade00ae967e
Jonathan Garvin <jgarvin@wwidea.org>
2011-07-20 11:56:49 -0600

    update to jQuery

--- app/helpers/feedbacker_helper.rb
+++ app/helpers/feedbacker_helper.rb
@@ -1,9 +1,9 @@
 module FeedbackerHelper
   def show_feedback
-    "$('expanded_feedback_form').show(); $('compressed_feedback_form_tab').hide();"
+    "$('#expanded_feedback_form').show(); $('#compressed_feedback_form_tab').hide();"
   end
   
   def hide_feedback
-    "$('compressed_feedback_form_tab').show(); $('expanded_feedback_form').hide();"
+    "$('#compressed_feedback_form_tab').show(); $('#expanded_feedback_form').hide();"
   end
 end
