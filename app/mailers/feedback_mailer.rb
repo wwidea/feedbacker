9ec9e62432962ac121e6bda95617c6ce28436c80
Aaron Baldwin <baldwina@wwidea.org>
2013-07-15 15:50:53 -0600

    reformat to improve readability

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,6 +1,11 @@
 class FeedbackMailer < ActionMailer::Base
   def feedback(remark)
     @remark = remark
-    mail(:to => Feedbacker::EMAIL_TO, :from => Feedbacker::EMAIL_TO, :reply_to => remark.user.email, :subject => "#{(defined? Feedbacker::EMAIL_PREFIX) ? Feedbacker::EMAIL_PREFIX : '[FEEDBACKER]'} Feedback Notification")
+    mail(
+      to:         Feedbacker::EMAIL_TO,
+      from:       Feedbacker::EMAIL_TO,
+      reply_to:   remark.user.email,
+      subject:    "#{(defined? Feedbacker::EMAIL_PREFIX) ? Feedbacker::EMAIL_PREFIX : '[FEEDBACKER]'} Feedback Notification"
+    )
   end
 end
