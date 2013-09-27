67c99cb3cfa78233e5c2fc41c818592a437a08e2
Aaron Baldwin <baldwina@wwidea.org>
2013-09-27 14:04:23 -0600

    update to work with rails 4 strong parameters

--- lib/feedbacker/remarks_controller_methods.rb
+++ lib/feedbacker/remarks_controller_methods.rb
@@ -1,7 +1,7 @@
 module Feedbacker
   module RemarksControllerMethods
     def create 
-      @remark = Remark.new(params[:remark].merge(:user_id => current_user.id))
+      @remark = Remark.new(remark_params.merge(:user_id => current_user.id))
       @remark.source_url = request.env['HTTP_REFERER']
       if @remark.save
         FeedbackMailer.feedback(@remark).deliver
@@ -9,6 +9,12 @@ module Feedbacker
           format.js
         end
       end
+    end
+    
+    private
+    
+    def remark_params
+      params.respond_to?(:permit) ? params.require(:remark).permit(:content) : params[:remark]
     end
   end
 end
