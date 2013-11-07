d2dd94c4ac7b5c1d32b4939812e17f4b686e43ca
Aaron Baldwin <baldwina@wwidea.org>
2013-11-07 10:41:08 -0700

    remove remark_observer configuration line

--- lib/feedbacker.rb
+++ lib/feedbacker.rb
@@ -2,7 +2,6 @@ Dir[File.join(File.dirname(__FILE__), '/feedbacker/*.rb')].each {|file| require 
 
 module Feedbacker
   class Engine < ::Rails::Engine
-    config.active_record.observers = :remark_observer
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
     end
